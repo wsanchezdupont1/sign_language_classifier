63f3f851ddbace98e6d34a01c787bb5ceedee02c
wsanchezdupont1 <wsanchezdupont@g.hmc.edu>
2020-01-22 11:34:35 -0700

    created file for making pseudodatset

---
+++ make_pseudodataset.py
@@ -0,0 +1,12 @@
+"""
+wsanchezdupont@gmail.com
+
+make_pseudo_datasets.py
+
+
+1. Generate a .csv list of indices for each sample based on sorted filenames
+2. Randomly sample validation split of indices
+3. Write .csv containing train vs val assignment
+4. Make dataset class that gets its indices from .csv
+
+"""
